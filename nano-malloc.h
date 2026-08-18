ecf37b36a8028599645b7fb75e5b85904d6f6127
Charlie Gordon <github@chqrlie.org>
2026-08-18 11:17:43 +0200

    Library: no longer clear allocated blocks * arena is zero initialized, hence blocks carved from the end are cleared * clear last block in free and extra chunk in realloc on the last block

--- nano-malloc.h
+++ nano-malloc.h
@@ -2,34 +2,34 @@
 // stdlib.h
 #define ARENA_LENGTH 4194304 // 4MB should suffice for everyone
 unsigned char arena[ARENA_LENGTH];
-size_t arena_top;
-size_t arena_last;
+size_t arena_top, arena_last;
 void *malloc(size_t size) {
-    size = (size + 15) & ~15;
+    size = (size + 7) & ~7;
     if (arena_top + size > sizeof(arena)) return 0;
     arena_last = arena_top;
     arena_top += size;
     return arena + arena_last;
 }
 void free(void *p) {
-    if (p && p == arena + arena_last) { arena_top = arena_last; }
+    if (p == arena + arena_last) {
+        memset(p, 0, arena_top - arena_last); arena_top = arena_last;
+    }
 }
 void *calloc(size_t nmemb, size_t size) {
-    size *= nmemb;
-    void *p = malloc(size);
-    if (p) return memset(p, 0, size);
-    return p;
+    return malloc(size * nmemb);
 }
 void *realloc(void *p, size_t size) {
-    if (!p) return malloc(size);
+    size = (size + 7) & ~7;
     if (p == arena + arena_last) {
-        arena_top = arena_last + ((size + 15) & ~16);
+        size_t last_top = arena_top;
+        arena_top = arena_last + size;
+        if (arena_top < last_top) { memset(arena + arena_top, 0, last_top - arena_top); }
         return p;
     }
     void *new_p = malloc(size);
-    if (new_p) {
+    if (new_p && p) {
+        // size may be larger than original block size but OK
         return memcpy(new_p, p, size);
-        free(p);
     }
     return new_p;
 }
